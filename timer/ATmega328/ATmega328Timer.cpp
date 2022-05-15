f08dd99dd076d1f46b706b6480164a9050270899
Damon Ball <daball86@gmail.com>
2022-05-15 11:20:58 -0400

    Adding timer functionalities needed for servo driver

--- timer/ATmega328/ATmega328Timer.cpp
+++ timer/ATmega328/ATmega328Timer.cpp
@@ -58,6 +58,20 @@ namespace Timer{
         [EXTERNAL_RISING] = 0,
     };
 
+    const static uint16_t PRESCALE_INT[] = 
+    {
+        [PRESCALE_OFF] = 0,
+        [PRESCALE_1] = 1,
+        [PRESCALE_8] = 8,
+        [PRESCALE_32] = 32,
+        [PRESCALE_64] = 64,
+        [PRESCALE_128] = 128,
+        [PRESCALE_256] = 256,
+        [PRESCALE_1024] = 1024,
+        [EXTERNAL_FALLING] = 0,
+        [EXTERNAL_RISING] = 0,
+    };
+
     Atmega328Timer::Atmega328Timer(Timer timer,
                                    TimerMode mode,
                                    TimerPrescaler prescaler,
@@ -66,7 +80,8 @@ namespace Timer{
         timer_(timer),
         mode_(mode),
         prescaler_(prescaler),
-        top_(top)
+        top_(top),
+        enabled_(false)
     {
         switch (timer_)
         {
@@ -99,6 +114,37 @@ namespace Timer{
             }
         }
         setCompAInterrupt(timer, interrupt);
+    }
+
+    void Atmega328Timer::setTop(uint16_t newTop)
+    {
+        switch (timer_)
+        {
+            case TIMER_0:
+            {
+                // Set the compare A top value (8 bit)
+                top_ = (uint8_t)newTop;
+                OCR0A = (uint8_t)top_;
+                break;
+            }
+
+            case TIMER_1:
+            {
+                // Set the compare A top value (16 bit)
+                top_ = newTop;
+                OCR1A = top_;
+                break;
+            }
+
+            default:
+            case TIMER_2:
+            {
+                // Set the compare A top value (8 bit)
+                top_ = (uint8_t)newTop;
+                OCR2A = (uint8_t)top_;
+                break;
+            }
+        }
     }
 
     void Atmega328Timer::initialize()
@@ -156,7 +202,7 @@ namespace Timer{
         }
 
         // Enable timer interrupt
-        enableCompAInterrupt(timer_);
+        disable();
     }
 
     void Atmega328Timer::setCompAInterrupt(Timer timer, void (*interrupt)(void)){
@@ -189,19 +235,78 @@ namespace Timer{
         *TIM_INTERRUPT_REG[timer] |= COMP_A_INTERRUPT_VAL;
     }
 
-    void Atmega328Timer::disableCompAInterrupt(Timer timer){
+    void Atmega328Timer::disableCompAInterrupt(Timer timer)
+    {
         *TIM_INTERRUPT_REG[timer] &= ~COMP_A_INTERRUPT_VAL;
     }
 
+    void Atmega328Timer::enable()
+    {
+        enableCompAInterrupt(timer_);
+        enabled_ = true;
+    }
+
+    void Atmega328Timer::disable()
+    {
+        disableCompAInterrupt(timer_);
+        enabled_ = false;
+    }
+
+    void Atmega328Timer::setInterrupt(void (*interrupt)(void))
+    {
+        setCompAInterrupt(timer_, interrupt);
+    }
+
+    void Atmega328Timer::setPeriodTics(uint16_t tics)
+    {
+        setTop(tics);
+    }
+
+    uint16_t Atmega328Timer::microSecondsToTics(uint32_t microseconds)
+    {
+        uint32_t divisor = (uint32_t)PRESCALE_INT[prescaler_] * (uint32_t)(1000000 / microseconds);
+        return (F_CPU / divisor) - 1;
+    }
+
+    void Atmega328Timer::reset()
+    {
+        switch (timer_)
+        {
+            case TIMER_0:
+                TCNT0 = 0;
+                break;
+
+            case TIMER_1:
+                TCNT1 = 0;
+                break;
+
+            case TIMER_2:
+                TCNT2 = 0;
+                break;
+        
+            default:
+                break;
+        }
+    }
+
     ISR(TIMER0_COMPA_vect){
-        Timer0CompareAInterrupt();
+        if (Timer0CompareAInterrupt != nullptr)
+        {
+            Timer0CompareAInterrupt();
+        }
     }
 
     ISR(TIMER1_COMPA_vect){
-        Timer1CompareAInterrupt();
+        if (Timer1CompareAInterrupt != nullptr)
+        {
+            Timer1CompareAInterrupt();
+        }
     }
 
     ISR(TIMER2_COMPA_vect){
-        Timer2CompareAInterrupt();
+        if (Timer2CompareAInterrupt != nullptr)
+        {
+            Timer2CompareAInterrupt();
+        }
     }
 }
