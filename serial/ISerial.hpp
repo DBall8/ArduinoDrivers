1df5bb8228646a9e95dbf25d8c1564be3f8b3b72
Damon Ball <daball86@gmail.com>
2022-05-15 11:21:50 -0400

    Adding readLine for serial

--- serial/ISerial.hpp
+++ serial/ISerial.hpp
@@ -2,6 +2,7 @@
 #define ISERIAL_HPP
 
 #include <stdint.h>
+#include "drivers/timer/SoftwareTimer.hpp"
 
 namespace SerialComm
 {
@@ -20,6 +21,13 @@ namespace SerialComm
             virtual void write(const uint8_t* buff, uint16_t numBytes) = 0;
             virtual uint16_t read(uint8_t* buff, uint16_t numBytes) = 0;
             virtual void flush(){}
+            virtual void flushRx(){}
+            virtual void flushTx(){}
+
+            void setTimeoutTimer(Timer::SoftwareTimer* pTimer)
+            {
+                pTimeoutTimer_ = pTimer;
+            }
 
             virtual void write(const char* buff, uint16_t numBytes)
             {
@@ -30,6 +38,48 @@ namespace SerialComm
             {
                 return read((uint8_t*) buff, numBytes);
             }
+
+            /**
+             * Read data until a newline character is reached
+             * @param   buff        Buffer for received data to be copied to
+             * @param   numBytes    Maximum number of received data to copy to buff
+             * @return  Number of bytes copied to buff
+             */
+            virtual uint16_t readLine(uint8_t* buff, uint16_t maxBytes, uint16_t timeoutMs)
+            {
+                if ((pTimeoutTimer_ != nullptr) &&
+                    timeoutMs != 0)
+                {
+                    pTimeoutTimer_->setPeriodMs(timeoutMs);
+                    pTimeoutTimer_->enable();
+                }
+
+                uint16_t bytesRead = 0;
+                while (bytesRead < maxBytes)
+                {
+                    if ((pTimeoutTimer_ != nullptr) &&
+                        (timeoutMs > 0) &&
+                        pTimeoutTimer_->hasOneShotPassed())
+                    {
+                        return bytesRead;
+                    }
+
+                    if (read(&(buff[bytesRead]), 1))
+                    {
+                        bytesRead++;
+
+                        if (buff[bytesRead-1] == '\n')
+                        {
+                            buff[bytesRead-1] = '\0';
+                            return bytesRead;
+                        }
+                    }
+                }
+                return bytesRead;
+            }
+
+        protected:
+            Timer::SoftwareTimer* pTimeoutTimer_ = nullptr;
     };
 }
 
